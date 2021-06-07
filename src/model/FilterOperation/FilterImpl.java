c4b488a1c6ee06fa55f6cb51677cd11eb8dec0ff
srhtakeba <takedabyrnesarah@gmail.com>
2021-06-07 16:40:40 -0400

    commit

--- src/model/FilterOperation/FilterImpl.java
+++ src/model/FilterOperation/FilterImpl.java
@@ -46,9 +46,15 @@ public abstract class FilterImpl implements FilterOperation {
     for (int i = 0; i < channelMatrix.length; i++) {
       for (int j = 0; j < channelMatrix[0].length; j++) {
         double appliedResult = 0;
+
         for (int k = 0; k < kernel.length; k++) {
           for (int d = 0; d < kernel[0].length; d++) {
+            /*
             if (k < channelMatrix.length && j < channelMatrix[0].length && k >=0 && d>= 0) {
+              appliedResult += (channelMatrix[i][j].getValue()) * kernel[k][d];
+            }*/
+            if ( ((kernel.length-i)-k<kernel.length) && ((kernel.length-i)-k >= 0)
+                && ((kernel[i].length-j)-d<kernel[i].length) && ((kernel[i].length-j)-d>=0)) {
               appliedResult += (channelMatrix[i][j].getValue()) * kernel[k][d];
             }
           }
