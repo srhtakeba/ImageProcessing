30cece3c2492d2ec4a31221905c47d56b17c8db3
srhtakeba <takedabyrnesarah@gmail.com>
2021-06-08 15:51:10 -0400

    added example checkerboard and rainbow images

--- test/ExportCheckerboardTest.java
+++ test/ExportCheckerboardTest.java
@@ -8,33 +8,33 @@ public class ExportCheckerboardTest {
   InstaImage checkerboard = new ImageImpl().makeCheckerBoard(20);
   InstagramModel model = new InstagramModelImpl(checkerboard);
 
-//  @Test
-//  public void testExportPPM() {
-//    model = new InstagramModelImpl(checkerboard);
-//    model.exportAsPPM("checkerboard");
+  @Test
+  public void testExportPPM() {
+    model = new InstagramModelImpl(checkerboard);
+    model.exportAsPPM("checkerboard");
 //    model.save();
 //    model.filter("blur");
 //    model.exportAsPPM("checkerboardBlur");
 //    model.retrieve();
 //    model.exportAsPPM("checkerboardRetrieved");
-//  }
-//
-//  @Test
-//  public void testExportRainbowPPM() {
-//    model = new InstagramModelImpl(new ImageImpl().makeRainbow(30, 70));
-//    model.exportAsPPM("rainbow");
-//    model.filter("sharpen");
-//    model.exportAsPPM("rainbowSharpen");
-//  }
-
+  }
 
   @Test
-  public void testImportPPMSaveAndRetrieve() {
-    InstagramModel model = new InstagramModelImpl();
-    model.readPPM("fish.ppm");
-    model.save();
-    model.transform("sepia");
-    model.exportAsPPM("fishSepia");
+  public void testExportRainbowPPM() {
+    model = new InstagramModelImpl(new ImageImpl().makeRainbow(30, 70));
+    model.exportAsPPM("rainbow");
+//    model.filter("sharpen");
+//    model.exportAsPPM("rainbowSharpen");
   }
+
+
+//  @Test
+//  public void testImportPPMSaveAndRetrieve() {
+//    InstagramModel model = new InstagramModelImpl();
+//    model.readPPM("fish.ppm");
+//    model.save();
+//    model.transform("sepia");
+//    model.exportAsPPM("fishSepia");
+//  }
 
 }
