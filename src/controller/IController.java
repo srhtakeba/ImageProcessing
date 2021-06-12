55035286d412b4d6d1430199dfe5005b706bf26a
srhtakeba <takedabyrnesarah@gmail.com>
2021-06-12 16:21:20 -0400

    added main

--- src/controller/IController.java
+++ src/controller/IController.java
@@ -1,13 +1,9 @@
 package controller;
 
-import java.util.Scanner;
-import model.InstagramModel;
-
 public interface IController {
 
   /**
    * Dispatch operations depending on the
-   * @param model
    */
   void go();
 
---
+++ src/main/MainInstagram.java
@@ -0,0 +1,19 @@
+package main;
+
+import controller.Controller;
+import controller.IController;
+import java.io.InputStreamReader;
+
+public class MainInstagram {
+  /**
+   * Runs a game of SimpleFreecellModel using a SimpleFreecellController in the console.
+   *
+   * @param args command line input.
+   */
+  public static void main(String[] args) {
+    Readable in = new InputStreamReader(System.in);
+    IController controller = new Controller(in);
+    controller.go();
+  }
+
+}
