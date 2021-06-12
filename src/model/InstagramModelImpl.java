9a6e516514897f4aeb0aa5489b13fb0b0ead0441
srhtakeba <takedabyrnesarah@gmail.com>
2021-06-12 09:46:17 -0400

    Notes for assignment 6

--- src/model/InstagramModelImpl.java
+++ src/model/InstagramModelImpl.java
@@ -13,6 +13,34 @@ import model.pixel.Pixel;
 import model.transform.GreyscaleOperation;
 import model.transform.SepiaToneOperation;
 import model.transform.TransformOperation;
+/*
+ASSIGNMENT 6 NOTES:
+- To WRITE to files:
+  - Use ImageIO.write(RenderedImage im, String formatName, File output)
+    - Use BufferedImage(int width, int height, TYPE_INT_RGB).setRGB(int x, int y, int rgb)
+      Where BufferedImage is a child class of RenderedImage and int rgb is a merged integer.
+      We would use ((red<<16)|(green<<8)|blue) to convert our rgb values into a merged int.
+        - or we could use new Color(int r, int g, int b).getRGB(), which would do the same
+    - Use getWriterFormatNames() to get the list of string format names. Seems like Strings such as
+      "jpeg" can be used.
+    - Use File(String pathname)
+- to READ files
+  - Use ImageIO.read(File input) which returns a BufferedImage
+    - Again, use new File(String pathname)
+  - From the resulting BufferedImage, .getHeight(), .getWidth(), .getRGB(int x, int y)
+  - Construct a new Color(img.getRGB(int x, int y)).getR() etc to get the individual rgb values
+    as ints.
+ */
+
+/** TO DO:
+ * - Make a controller that can pass data onto the model for file writing and stuff
+ * - Make a view that can do like choose between
+ *    1) Write interactively
+ *    2) Load a command file
+ *   And then it would read the commands from the user and make layers and all this stuff
+ * - We need to be careful about separating all of these things
+ * - Make a new model interface that extends InstagramModel that adds the layer feature.
+ */
 
 /**
  * An implementation of {@code InstagramModel} that uses the {@code InstaImage} object type to
