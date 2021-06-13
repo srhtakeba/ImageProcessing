f98883760b04a227691272d26c7fefa8241aa4e3
katsuuuu <53023666+katsuuuu@users.noreply.github.com>
2021-06-13 16:59:39 -0400

    1650 commit

--- src/main/MainInstagram.java
+++ src/main/MainInstagram.java
@@ -2,7 +2,11 @@ package main;
 
 import controller.Controller;
 import controller.IController;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class MainInstagram {
@@ -11,8 +15,15 @@ public class MainInstagram {
    * @param args command line input.
    */
   public static void main(String[] args) {
-    Readable in = new InputStreamReader(System.in);
-    //Readable in = new FileInputStream("script.txt");
+    //Readable in = new InputStreamReader(System.in);
+    Readable in;
+    try {
+      File script = new File("script.txt");
+      InputStream inStream = new FileInputStream(script);
+      in = new InputStreamReader(inStream);
+    } catch (IOException e) {
+      throw new IllegalStateException("Invalid script to read");
+    }
     IController controller = new Controller(in);
     controller.go();
 
